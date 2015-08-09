794974608b4d1d073031d67bf220827d908f4cf4
JaviSG91 <javisago91@gmail.com>
2015-08-09 19:22:18 +0200

    profilecompleto

--- app/src/main/java/com/example/lrdzero/tfg/ProfileActivity.java
+++ app/src/main/java/com/example/lrdzero/tfg/ProfileActivity.java
@@ -1,31 +1,110 @@
 package com.example.lrdzero.tfg;
 
+import android.annotation.TargetApi;
 import android.app.Activity;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.v7.app.AlertDialog;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
 
+import java.io.File;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 
 import static android.app.PendingIntent.getActivity;
+import static com.example.lrdzero.tfg.R.id.icamara;
 
 
 public class ProfileActivity extends Activity {
     ArrayList<Integer> Discapacidades = new ArrayList<>();
+    private static final int CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 100;
+    public static final int MEDIA_TYPE_IMAGE = 1;
+    private static final int PICK_IMAGE=200;
+    private Uri fileUri;
 
+
+
+    @TargetApi(Build.VERSION_CODES.FROYO)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
         Button discButton = (Button)findViewById(R.id.discapacidades);
+
         ImageView estadisticas = (ImageView)findViewById(R.id.imageestadisticas);
+        ImageView foto = (ImageView)findViewById(R.id.foto);
+
+        fileUri = getOutputMediaFileUri(MEDIA_TYPE_IMAGE);
+        foto.setImageURI(fileUri);
+
+
+        AlertDialog.Builder builder2 = new AlertDialog.Builder(this);
+        LayoutInflater inflater = getLayoutInflater();
+        builder2.setView(inflater.inflate(R.layout.capturafoto, null));
+        builder2.setTitle("Elija una opcion")
+                .setNegativeButton("Cancelar", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int id) {
+
+                    }
+                });
+
+        final AlertDialog dialog = builder2.create();
+
+
+
+
+
+        dialog.setOnShowListener(new DialogInterface.OnShowListener() {
+
+            @Override
+            public void onShow(DialogInterface dialogI) {
+                    ImageView camara= (ImageView)dialog.findViewById(icamara);
+                    ImageView galeria= (ImageView)dialog.findViewById(R.id.igallery);
+                    camara.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                          //  Log.i("Camara", String.valueOf(fileUri));
+                           // Log.i("Camara", MediaStore.EXTRA_OUTPUT);
+                            Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                            intent.putExtra(MediaStore.EXTRA_OUTPUT, fileUri); // set the image file name
+
+                            startActivityForResult(intent, CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE);
+
+                        }
+                    });
+
+                    galeria.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                           // Log.i("Camara", String.valueOf(fileUri));
+                            //Log.i("Camara", MediaStore.EXTRA_OUTPUT);
+                            Intent intent = new Intent();
+                            intent.setType("image/*");
+                            intent.setAction(Intent.ACTION_GET_CONTENT);
+                            startActivityForResult(Intent.createChooser(intent, "Selecciona una foto"), PICK_IMAGE);
+
+                        }
+                    });
+                   // ((AlertDialog)dialog).getButton(AlertDialog.BUTTON_POSITIVE).setVisibility(View.INVISIBLE);
+
+            }
+        });
+
+        //dialog.show();
         final AlertDialog.Builder builder = new AlertDialog.Builder(this);
         discButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -76,6 +155,16 @@ public class ProfileActivity extends Activity {
             }
         });
 
+        foto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // create Intent to take a picture and return control to the calling application
+                dialog.show();
+
+            }
+        });
+        // start the image capture Intent
+
         estadisticas.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -110,6 +199,82 @@ public class ProfileActivity extends Activity {
         return super.onOptionsItemSelected(item);
     }
 
+    /** Create a file Uri for saving an image or video */
+    private static Uri getOutputMediaFileUri(int type){
+        return Uri.fromFile(getOutputMediaFile(type));
+    }
+
+    /** Create a File for saving an image or video */
+    @TargetApi(Build.VERSION_CODES.FROYO)
+    private static File getOutputMediaFile(int type){
+        // To be safe, you should check that the SDCard is mounted
+        // using Environment.getExternalStorageState() before doing this.
+
+        File mediaStorageDir = new File(Environment.getExternalStoragePublicDirectory(
+                Environment.DIRECTORY_PICTURES), "RutasGranada");
+        // This location works best if you want the created images to be shared
+        // between applications and persist after your app has been uninstalled.
+
+        // Create the storage directory if it does not exist
+        if (! mediaStorageDir.exists()){
+            if (! mediaStorageDir.mkdirs()){
+                Log.d("Camara", "failed to create directory");
+                return null;
+            }
+        }
+
+
+
+        // Create a media file name
+        //String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        File mediaFile;
+        if (type == MEDIA_TYPE_IMAGE){
+            mediaFile = new File(mediaStorageDir.getPath() + File.separator +
+                    "fotoperfil.jpg");
+
+        }else {
+            return null;
+        }
+
+        return mediaFile;
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        ImageView iv = (ImageView)findViewById(R.id.foto);
+        if (requestCode == CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE) {
+            if (resultCode == RESULT_OK) {
+                // Image captured and saved to fileUri specified in the Intent
+                //Log.i("Camara", String.valueOf(data));
+               //Toast.makeText(this, "Image saved to:\n" +
+                    //  fileUri, Toast.LENGTH_LONG).show();
+
+                iv.setImageURI(fileUri);
+                finish();
+                startActivity(getIntent());
+            } else if (resultCode != RESULT_CANCELED){
+                Toast.makeText(this,"Error al capturar la imagen", Toast.LENGTH_LONG).show();
+                // Image capture failed, advise user
+            }
+        }
+        else if(requestCode == PICK_IMAGE) {
+
+            if (resultCode == RESULT_OK) {
+                // Image captured and saved to fileUri specified in the Intent
+                //Log.i("Camara", String.valueOf(data));
+
+
+                 iv.setImageURI(data.getData());
+
+
+            } else if (resultCode != RESULT_CANCELED) {
+                // User cancelled the image capture
+                Toast.makeText(this,"Error al recoger la imagen", Toast.LENGTH_LONG).show();
+
+            }
+        }
+
+    }
 
 
 
