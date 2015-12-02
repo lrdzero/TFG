3c08e932d69cd40aabce3e9448bba1b3c0265a3a
Javier Salido Gómez <javisago91@gmail.com>
2015-12-02 23:58:12 +0100

    final escena

---
+++ app/src/main/java/com/example/lrdzero/tfg/Finalruta.java
@@ -0,0 +1,13 @@
+package com.example.lrdzero.tfg;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Finalruta extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_finalruta);
+    }
+}
