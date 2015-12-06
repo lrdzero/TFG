ddbb989bf50999bbb8d074b759496d92b9c9825d
Javier Salido Gómez <javisago91@gmail.com>
2015-12-06 10:50:21 +0100

    musica ON

--- app/src/main/java/com/example/lrdzero/tfg/Finalruta.java
+++ app/src/main/java/com/example/lrdzero/tfg/Finalruta.java
@@ -2,23 +2,127 @@ package com.example.lrdzero.tfg;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.view.animation.Animation;
+import android.view.animation.AnimationSet;
 import android.view.animation.AnimationUtils;
+import android.view.animation.TranslateAnimation;
+import android.widget.ImageView;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import java.util.Random;
 
 public class Finalruta extends AppCompatActivity {
+    TextView tv;
+    ImageView iv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_finalruta);
 
-        Animation af = AnimationUtils.loadAnimation(this,R.anim.animfinal);
+        final Animation af = AnimationUtils.loadAnimation(this,R.anim.animfinal);
+        tv = (TextView)findViewById(R.id.textitem);
+        iv = (ImageView) findViewById(R.id.item);
+
 
         RelativeLayout avatar = (RelativeLayout)findViewById(R.id.Avatar);
         af.setFillEnabled(true);
         af.setFillAfter(true);
+
+        Animation ocultar = AnimationUtils.loadAnimation(this,R.anim.abc_fade_out);
+        ocultar.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+        ocultar.setDuration(2000);
+        Animation mostrar= AnimationUtils.loadAnimation(this,R.anim.abc_fade_in);
+        mostrar.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+        mostrar.setDuration(2000);
+
+
+        af.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                tv.setVisibility(View.VISIBLE);
+                iv.setVisibility(View.VISIBLE);
+                tv.startAnimation(af);
+                iv.startAnimation(af);
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+
         avatar.startAnimation(af);
 
+    }
+
+    public AnimationSet habla(){
+
+        Animation habla = new TranslateAnimation(Animation.RELATIVE_TO_SELF,0,Animation.RELATIVE_TO_SELF,(float)0,Animation.RELATIVE_TO_SELF,0,Animation.RELATIVE_TO_SELF,(float)0.03);
+        habla.setDuration(500);
+        habla.setRepeatCount(Animation.INFINITE);
+        habla.setRepeatMode(Animation.REVERSE);
+        habla.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+                Random rand = new Random();
+
+                // nextInt is normally exclusive of the top value,
+                // so add 1 to make it inclusive
+                int randomNum = rand.nextInt((300 - 50) + 1) + 50;
+                animation.setDuration(randomNum);
+            }
+        });
+
+        AnimationSet as = new AnimationSet(true);
+        as.addAnimation(habla);
+        return as;
     }
 }
